fd33dbd7801ab71f81f30db3fbba5bb01918458d
Nicolas Erni <gann@zhaw.ch>
2023-08-09 16:44:07 +0200

    added readme to the created projects

---
+++ templates/README.md
@@ -0,0 +1,24 @@
+# Python Tool Competition Implementation Using $readable_name
+
+Uses the python-tool-competition-2024 to generate tests using the
+$readable_name.
+
+For more information see
+<https://github.com/ThunderKey/python-tool-competition-2024/>.
+
+## Installation
+
+* Install [poetry](https://python-poetry.org/)
+* Run `poetry install`
+
+## Development
+
+The entry point called by `python-tool-competition-2024` is the `build_test`
+method in `$module_name/generator.py`.
+
+## Calculating Metrics
+
+Run `poetry run python-tool-competition-2024 run <generator name>`.
+
+With `poetry run python-tool-competition-2024 run -h` you can find out what
+generators were detected.
--- tests/cli/test_init.py
+++ tests/cli/test_init.py
@@ -166,7 +166,32 @@ def test_init_with_confirm(tmp_path: Path, monkeypatch: pytest.MonkeyPatch) -> N
 
     assert _list_dir(parent_dir) == {
         "python-tool-competition-2024-some-generator": {
-            "README.md": "",
+            "README.md": """\
+# Python Tool Competition Implementation Using Some Generator
+
+Uses the python-tool-competition-2024 to generate tests using the
+Some Generator.
+
+For more information see
+<https://github.com/ThunderKey/python-tool-competition-2024/>.
+
+## Installation
+
+* Install [poetry](https://python-poetry.org/)
+* Run `poetry install`
+
+## Development
+
+The entry point called by `python-tool-competition-2024` is the `build_test`
+method in `python_tool_competition_2024_some_generator/generator.py`.
+
+## Calculating Metrics
+
+Run `poetry run python-tool-competition-2024 run <generator name>`.
+
+With `poetry run python-tool-competition-2024 run -h` you can find out what
+generators were detected.
+""",
             "pyproject.toml": _EXAMPLE_PYPROJECT
             + """\
 
@@ -268,7 +293,32 @@ def test_init_with_tmp_file(tmp_path: Path) -> None:
     assert _list_dir(parent_dir) == {
         "outer_existing.py": "",
         "python-tool-competition-2024-testgen": {
-            "README.md": "",
+            "README.md": """\
+# Python Tool Competition Implementation Using TestGen
+
+Uses the python-tool-competition-2024 to generate tests using the
+TestGen.
+
+For more information see
+<https://github.com/ThunderKey/python-tool-competition-2024/>.
+
+## Installation
+
+* Install [poetry](https://python-poetry.org/)
+* Run `poetry install`
+
+## Development
+
+The entry point called by `python-tool-competition-2024` is the `build_test`
+method in `python_tool_competition_2024_testgen/generator.py`.
+
+## Calculating Metrics
+
+Run `poetry run python-tool-competition-2024 run <generator name>`.
+
+With `poetry run python-tool-competition-2024 run -h` you can find out what
+generators were detected.
+""",
             "pyproject.toml": _EXAMPLE_PYPROJECT
             + """\
 
